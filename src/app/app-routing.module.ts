3d7f717c7967256fd3830654d0a43dcf9a8150ef
a19c704 <simon.batschkuhn@rewe-group.com>
2023-11-09 12:15:17 +0100

    graph page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomePageComponent} from "./components/home-page/home-page.component";
-import {ErrorPageComponent} from "./error-page/error-page.component";
+import {ErrorPageComponent} from "./components/error-page/error-page.component";
 
 const routes: Routes = [
   { path: "", redirectTo:"/home", pathMatch: 'full'},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterOutlet } from "@angular/router";
-import { AppRoutingModule } from './app-routing.module';
-import { HomePageComponent } from './components/home-page/home-page.component';
-import { ErrorPageComponent } from './error-page/error-page.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterOutlet} from "@angular/router";
+import {AppRoutingModule} from './app-routing.module';
+import {HomePageComponent} from './components/home-page/home-page.component';
+import {ErrorPageComponent} from './components/error-page/error-page.component';
 import {MatIconModule} from "@angular/material/icon";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule} from "@angular/material/toolbar";
+import {GraphPageComponent} from './components/graph-page/graph-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     ErrorPageComponent,
+    GraphPageComponent,
   ],
   imports: [
     BrowserModule,
@@ -31,4 +33,5 @@ import {MatToolbarModule} from "@angular/material/toolbar";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
