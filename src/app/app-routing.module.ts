c644f9a59663d1065faf12338451765e312f947a
a19c704 <simon.batschkuhn@rewe-group.com>
2023-11-12 16:02:29 +0100

    graph page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomePageComponent} from "./components/home-page/home-page.component";
 import {ErrorPageComponent} from "./components/error-page/error-page.component";
+import {GraphPageComponent} from "./components/graph-page/graph-page.component";
 
 const routes: Routes = [
   { path: "", redirectTo:"/home", pathMatch: 'full'},
   { path: "home", component: HomePageComponent },
   { path: "error", component: ErrorPageComponent },
+  { path: "graph", component: GraphPageComponent },
   { path: "**", redirectTo: "/error" },
 ];
 
